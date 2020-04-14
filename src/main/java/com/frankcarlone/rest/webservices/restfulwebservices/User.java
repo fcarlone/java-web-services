b058b8249e7a1082c937f61ac98248feb3561adc
fcarlone <frankcarlone@gmail.com>
2020-04-13 20:22:42 -0400

    Create User class

---
+++ src/main/java/com/frankcarlone/rest/webservices/restfulwebservices/User.java
@@ -0,0 +1,43 @@
+package com.frankcarlone.rest.webservices.restfulwebservices;
+
+import java.util.Date;
+
+public class User {
+	
+	private Integer id;
+	private String name;
+	private Date birthdate;
+	
+	public User(Integer id, String name, Date birthdate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthdate = birthdate;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public Date getBirthdate() {
+		return birthdate;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", birthdate=" + birthdate + "]";
+	}
+	
+	
+	
+}
