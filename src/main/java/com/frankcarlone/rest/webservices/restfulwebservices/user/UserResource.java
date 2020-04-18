c75fce1acfa7d2e0a15a47ab729fe8478a6cc2f5
fcarlone <frankcarlone@gmail.com>
2020-04-18 14:34:24 -0400

    Add validation to User

--- src/main/java/com/frankcarlone/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/frankcarlone/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,6 +3,8 @@ package com.frankcarlone.rest.webservices.restfulwebservices.user;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -39,7 +41,7 @@ public class UserResource {
 	
 //	Create and return an user
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User savedUser = service.save(user);
 		
 //		return created user
