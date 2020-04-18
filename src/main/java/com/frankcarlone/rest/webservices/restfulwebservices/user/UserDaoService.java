18dcd4a078563e301a6e7fbf17148b72c9142808
fcarlone <frankcarlone@gmail.com>
2020-04-18 12:08:53 -0400

    Create delete a user method

--- src/main/java/com/frankcarlone/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/frankcarlone/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -2,6 +2,7 @@ package com.frankcarlone.rest.webservices.restfulwebservices.user;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -44,4 +45,19 @@ public class UserDaoService {
 		}
 		return null;
 	}
+	
+//	Delete a user
+	public User deleteById(int id) {
+		Iterator<User> iterator = users.iterator();
+		while (iterator.hasNext()) {
+			User user = iterator.next();
+			
+			if(user.getId() == id) {
+				iterator.remove();
+				return user;
+			}
+		}
+		return null;
+	}
+	
 }
