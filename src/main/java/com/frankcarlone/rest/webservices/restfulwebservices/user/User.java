5ae1c252fe5c64b11db6f6854fcceb4ecc12f822
fcarlone <frankcarlone@gmail.com>
2020-04-18 20:21:14 -0400

    Create GET method for User

--- src/main/java/com/frankcarlone/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/frankcarlone/rest/webservices/restfulwebservices/user/User.java
@@ -2,11 +2,18 @@ package com.frankcarlone.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+@Entity
 public class User {
 	
+//	Primary Key and generated value
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=2, message="Name should have at least two characters")
@@ -14,6 +21,11 @@ public class User {
 	
 	@Past
 	private Date birthdate;
+	
+//	Default constructor
+	protected User() {
+
+	}
 	
 	public User(Integer id, String name, Date birthdate) {
 		super();
---
+++ src/main/java/com/frankcarlone/rest/webservices/restfulwebservices/user/UserJPAResource.java
@@ -0,0 +1,81 @@
+package com.frankcarlone.rest.webservices.restfulwebservices.user;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@RestController
+public class UserJPAResource {
+	
+//	Bring in UserDaoService
+	@Autowired
+	private UserDaoService service;
+	
+//	Use the JPA repository
+	@Autowired
+	private UserRepository userRepository;
+	
+//	Retrieve all users
+	@GetMapping("/jpa/users")
+	public List<User> retrieveAllUsers() {
+		return userRepository.findAll();
+	}
+	
+//	Retrieve a user by id
+	@GetMapping("/jpa/users/{id}")
+	public EntityModel<User> retrieveUser(@PathVariable int id) {
+		Optional<User> user = userRepository.findById(id);
+		
+		if(user.isEmpty()) {
+			throw new UserNotFoundException("id: " + id);
+		}
+			
+//		HATEOAS - return a link of the user
+		EntityModel<User> model = new EntityModel<User>(user.get());
+		WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		model.add(linkTo.withRel("all-users"));
+		
+		return model;
+	}
+	
+//	Create and return an user
+	@PostMapping("/jpa/users")
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+		User savedUser = service.save(user);
+		
+//		return created user
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
+	}
+	
+//	Delete a user by id
+	@DeleteMapping("/jpa/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.deleteById(id);
+		
+		if(user == null) {
+			throw new UserNotFoundException("id: " + id);
+		}
+	}
+
+}
